ef6792f67a5fc6102963f62b975c255e992f2442
ItSites <front3@itsites.com.ua>
2026-01-14 10:36:56 +0200

    change PORT

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
