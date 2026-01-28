35e981a912637dbb3c7fe6f4b9b4a6da580c248b
ItSites <front3@itsites.com.ua>
2026-01-28 20:28:50 +0200

    Trying to repair deployment

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 3000;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
+  const port = process.env.PORT || 3000;
+
+  console.log('PORT from env:', port);
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
